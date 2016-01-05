971b6e3b7973b3ee93465eb777397caab31edaad
volodya <volodya@intelex.ca>
2016-01-05 14:01:56 +0200

    service

--- lviv_parking/www/js/services.js
+++ lviv_parking/www/js/services.js
@@ -6,29 +6,25 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
+    name: 'парковка Арсен',
+    place: 200,
+    cost:,
+    open: '8:00 -23:00',
+    lat: 49.8227768,
+    lng: 23.9566649,
+    address:'вул. Патона, 37',
+    options:'disable, bike'
+
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
+    name: 'парковка Арсен',
+    place: 200,
+    cost:,
+    open: '8:00 -23:00',
+    lat: 49.862573,
+    lng: 24.005623,
+    address:'просп. В\'ячеслава Чорновола, 93',
+    options:'disable, bike'
   }];
 
   return {
